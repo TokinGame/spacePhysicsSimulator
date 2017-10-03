e77610f77d7e0fcbd9e518a8b486cf74024e9a17
davidmatyas <davimatyi@outlook.com>
2017-10-03 17:47:23 +0200

    GOIN TO MARS BOII

---
+++ android/src/hu/tokingame/physicscalculator/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.tokingame.physicscalculator;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.tokingame.physicscalculator.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
