c97a3d86097ca9b7642eb8d002f61aa1ce329b4d
tuskeb <tuskebganz@gmail.com>
2017-10-12 15:39:39 +0200

    Random

--- core/src/hu/tokingame/physicscalculator/Physics/MathUtils.java
+++ core/src/hu/tokingame/physicscalculator/Physics/MathUtils.java
@@ -1,10 +1,14 @@
 package hu.tokingame.physicscalculator.Physics;
 
+import java.util.Random;
+
 /**
  * Created by m on 10/5/2017.
  */
 
 public class MathUtils {
+    private static Random random = new Random();
+
     public static float dToF(double n){
         return (float) n;
     }
@@ -16,4 +20,9 @@ public class MathUtils {
     public static float sqr(float n){
         return dToF(Math.pow(fToD(n), 2));
     }
+
+    public static int getRandom(int a, int b){
+        return random.nextInt(b - a) + a;
+    }
+
 }
