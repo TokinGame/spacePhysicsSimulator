7f63381b8ad8ea0764c76eff9c9f81a855103e13
mrclsu <marcell.schuh@gmail.com>
2017-10-19 20:30:03 +0200

    Kúmpí lenni közép és targettttt
    #Spooke wíd # zoli hesteg

---
+++ core/src/hu/tokingame/physicscalculator/Simulation/Target.java
@@ -0,0 +1,10 @@
+package hu.tokingame.physicscalculator.Simulation;
+
+import hu.tokingame.physicscalculator.BaseClass.OneSpriteStaticActor;
+
+/**
+ * Created by M on 10/19/2017.
+ */
+
+public class Target extends OneSpriteStaticActor {
+}
