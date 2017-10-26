12915d46bb7962c8371f973d0263f47176e76ed9
mrclsu <marcell.schuh@gmail.com>
2017-10-26 19:50:29 +0200

    nhjbm hnjbg

--- core/src/hu/tokingame/physicscalculator/Menu/MenuStage.java
+++ core/src/hu/tokingame/physicscalculator/Menu/MenuStage.java
@@ -103,7 +103,7 @@ public class MenuStage extends MyStage {
             @Override
             protected void init() {
                 super.init();
-                setPosition(Globals.WORLD_WIDTH-this.getWidth()-10, 100);
+                setPosition(Globals.WORLD_WIDTH-this.getWidth()-10, 50);
                 addListener(new ClickListener(){
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
@@ -138,7 +138,7 @@ public class MenuStage extends MyStage {
             @Override
             protected void init() {
                 super.init();
-                setPosition(10, 250);
+                setPosition(10, 260);
                 setSize(150,150);
                 if(Globals.sounds) setTexture(Assets.manager.get(Assets.MUSIC_ON));
                 else Assets.manager.get(Assets.MUSIC_OFF);
